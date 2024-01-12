d2451465ee615b276dc661ab5710e14dad30c28b
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-01-13 00:32:12 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,9 @@
-const categoriesNumber = document.querySelectorAll('.item').length;
+const categoriesNumber = document.querySelectorAll(".item").length;
 console.log(categoriesNumber);
 
-function getList() {
-const categoriesList = document.querySelectorAll('h2');
-result = [];
+const categoriesList = document.querySelectorAll("h2");
+const result = [];
 for (var i = 0; i < categoriesList.length; i++) {
-    result.push(categoriesList[i].textContent);
+  result.push(categoriesList[i].textContent);
 }
 console.log(result);
-}
-
-
-
