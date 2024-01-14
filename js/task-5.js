569a16a05515a9be9f29538dc05d2bcc107fb05f
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-01-14 18:22:22 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -6,12 +6,12 @@ function getRandomHexColor() {
 
 const body = document.querySelector('body');
 
-// let colorName = document.querySelector('.color');
+let colorName = document.querySelector('.color');
 
 const changeColorBtn = document.querySelector(".change-color");
 changeColorBtn.addEventListener('click', onBtnClick);
 
 function onBtnClick () {
   body.style.backgroundColor = getRandomHexColor();
-  colorName.innerHTML = '';
+  colorName.innerHTML = `red`;
 }
