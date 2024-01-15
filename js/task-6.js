cd7e4c965cb2aaca814e8a1054c1538043393072
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-01-15 15:19:34 +0200

    upd

--- js/task-6.js
+++ js/task-6.js
@@ -7,12 +7,37 @@ function getRandomHexColor() {
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const inputBox = document.querySelector('input');
+const squaresDiv = document.getElementById("boxes");
 
 
 
 createBtn.addEventListener('click', onCreateBtn);
 
+
+function squareTemplate () {
+
+  return `<div class = square>
+  </div>`;
+
+};
+
+let squares = [];
+
+for (let squares >= 1 i++)
+
+
+function renderSquares () {
+  const squaresMarkup = squares.map(squareTemplate).join('');
+  squaresElem.insertAdjacentHTML("beforeend", squaresMarkup);
+}
+
+
+
+
 function onCreateBtn () {
+if (inputBox.value >= 1 && inputBox.value <=100) {
+  renderSquares();
+};
 
   inputBox.value = '';
 }
